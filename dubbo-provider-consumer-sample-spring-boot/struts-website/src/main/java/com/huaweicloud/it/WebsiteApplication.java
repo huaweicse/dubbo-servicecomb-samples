c06b723342d264aeef6c08dcb501f2a2a42c31ae
liubao <bismy@qq.com>
2020-10-28 19:18:03 +0800

    提供 struts 应用接入和部署的 demo

---
+++ dubbo-provider-consumer-sample-spring-boot/struts-website/src/main/java/com/huaweicloud/it/WebsiteApplication.java
@@ -0,0 +1,19 @@
+package com.huaweicloud.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class WebsiteApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebsiteApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteApplication.class, args);
+    }
+
+}
